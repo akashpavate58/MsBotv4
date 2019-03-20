437dcd0a0cfb9c6f7f80e888d3ffbb1771e83155
Akash Pavate <apavate@miraclesoft07.onmicrosoft.com>
2019-03-20 17:03:06 -0400

    added Dialog State

--- MsBotv4/Bot/Chatbot.cs
+++ MsBotv4/Bot/Chatbot.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Schema;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,8 @@ namespace MsBotv4.Bot
         private readonly ChatbotStateAccessor _accessor;
         private readonly BotServices _services;
 
+        private DialogSet _dialogs;
+
         public Chatbot(BotServices services, ChatbotStateAccessor accessor)
         {
             _accessor = accessor ?? throw new ArgumentNullException(nameof(accessor));
@@ -21,14 +24,50 @@ namespace MsBotv4.Bot
             {
                 throw new System.ArgumentException($"Invalid configuration. Please check your '.bot' file for a LUIS service named '{Constants.LuisKey}'.");
             }
+
+            _dialogs = new DialogSet(_accessor.ConversationDialogState);
+
+            // This array defines how the Waterfall will execute.
+            var waterfallSteps = new WaterfallStep[]
+            {
+                NameStepAsync,
+                AcquireNameStepAsync
+            };
+
+            // Add named dialogs to the DialogSet. These names are saved in the dialog state.
+            _dialogs.Add(new WaterfallDialog("details", waterfallSteps)); //(dialogId, dialogsInThatDialogId)
+            _dialogs.Add(new TextPrompt("name")); // (dialogId)
+            _dialogs.Add(new NumberPrompt<int>("age"));
+            _dialogs.Add(new ConfirmPrompt("confirm"));
+        }
+
+        private async Task<DialogTurnResult> NameStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.PromptAsync("name", new PromptOptions {
+                Prompt = MessageFactory.Text("Please enter your name")
+            }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> AcquireNameStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            string name = (string)stepContext.Result;
+            var state = await _accessor.FetchUserStateAsync(stepContext.Context, cancellationToken);
+            state.Username = name.Trim();
+            await _accessor.SaveUserStateAsync(stepContext.Context, state, cancellationToken);
+            await stepContext.Context.SendActivityAsync($"Thank you {name}");
+            return await stepContext.EndDialogAsync();
         }
 
         public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
         {
+            
             switch (turnContext.Activity.Type)
             {
                 case ActivityTypes.Message:
                     {
+                        var dialogContext = await _dialogs.CreateContextAsync(turnContext, cancellationToken);
+                        var results = await dialogContext.ContinueDialogAsync(cancellationToken);
+
                         ////This is the way to Get and Set State Values from Conversation State
                         //var state = await _accessor.FetchStateAsync(turnContext, cancellationToken);
                         //state.NameOfTheUser = "Akash";
@@ -38,28 +77,32 @@ namespace MsBotv4.Bot
 
 
                         // Check LUIS model
-                        var recognizerResult = await _services.LuisServices[Constants.LuisKey].RecognizeAsync(turnContext, cancellationToken);
-                        var topIntent = recognizerResult?.GetTopScoringIntent();
-
-                        switch (topIntent.Value.intent)
+                        if (results.Status == DialogTurnStatus.Empty)
                         {
-                            case Intent_None:
-                                await turnContext.SendActivityAsync("Sorry, I wasn't quite able to get that. Can you try re-phrasing your sentence, please?");
-                                break;
-                            case Intent_Greeting:
-                                await turnContext.SendActivityAsync("Hi, I am Juliet, a new recuit to BBB Auto Insurance Company. " +
-                                    "I am virtual assistant who's always ready to help you with your insurance needs");
-                                await turnContext.SendActivityAsync("How can I help you today?");
-                                break;
-                            case Intent_Capabilities:
-                                await turnContext.SendActivityAsync("I can help you get an insurance quote for your car.\nTry saying,\n*I need an insurance quote*");
-                                break;
-                            case Intent_InsuranceQuote:
-                                await turnContext.SendActivityAsync("Sure, I can help with getting an insurance quote");
-                                break;
-                            default:
-                                await turnContext.SendActivityAsync("I am unable to understand that. I am very sorry for not being able to assit you on this.");
-                                break;
+                            var recognizerResult = await _services.LuisServices[Constants.LuisKey].RecognizeAsync(turnContext, cancellationToken);
+                            var topIntent = recognizerResult?.GetTopScoringIntent();
+
+                            switch (topIntent.Value.intent)
+                            {
+                                case Intent_None:
+                                    await turnContext.SendActivityAsync("Sorry, I wasn't quite able to get that. Can you try re-phrasing your sentence, please?");
+                                    break;
+                                case Intent_Greeting:
+                                    await turnContext.SendActivityAsync("Hi, I am Juliet, a new recuit to BBB Auto Insurance Company. " +
+                                        "I am virtual assistant who's always ready to help you with your insurance needs");
+                                    await turnContext.SendActivityAsync("How can I help you today?");
+                                    break;
+                                case Intent_Capabilities:
+                                    await turnContext.SendActivityAsync("I can help you get an insurance quote for your car.\nTry saying,\n*I need an insurance quote*");
+                                    break;
+                                case Intent_InsuranceQuote:
+                                    await turnContext.SendActivityAsync("Sure, I can help with getting an insurance quote");
+                                    await dialogContext.BeginDialogAsync("details", null, cancellationToken);
+                                    break;
+                                default:
+                                    await turnContext.SendActivityAsync("I am unable to understand that. I am very sorry for not being able to assit you on this.");
+                                    break;
+                            } 
                         }
 
                         break;
@@ -70,6 +113,8 @@ namespace MsBotv4.Bot
                         break;
                     }
             }
+
+            await _accessor.ConversationState.SaveChangesAsync(turnContext, false, cancellationToken);
         }
 
         public const string Intent_InsuranceQuote = "InsuranceQuote";
